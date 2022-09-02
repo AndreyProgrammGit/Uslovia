1da9990d5a047f811ea923d850eb0d2cae2c2653
Andrey <andreytomson84@gmail.com>
2022-09-02 20:40:42 +0300

    End10

--- script.js
+++ script.js
@@ -41,19 +41,40 @@
 //     console.log("Я не сыт");
 // }
 
+// const humburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(humburger === 3 && cola && fries);
+
+// console.log(1 && 5);
+// console.log(1 && 0);
+// console.log(null && 5);
+// console.log(0 && 'asdasdasd');
+
+// if (humburger === 3 && cola === 1 && fries) {
+//     console.log("Все сыты");
+// }else {
+//     console.log("Мы уходим");
+// }
+
+
 const humburger = 3;
-const fries = 1;
+const fries = 3;
 const cola = 0;
+const nuggets = 2;
 
-console.log(humburger === 3 && cola && fries);
-
-console.log(1 && 5);
-console.log(1 && 0);
-console.log(null && 5);
-console.log(0 && 'asdasdasd');
-
-if (humburger === 3 && cola === 1 && fries) {
-    console.log("Все сыты");
+if (humburger === 3 && cola === 3 || fries === 3 && nuggets) {
+    console.log("Все довольны");
 }else {
     console.log("Мы уходим");
 }
+
+console.log(humburger === 3 && cola === 3 || fries === 3 && nuggets);
+
+
+let johnReport, alexReport, samReport, mariaReport = 'done';
+
+console.log(johnReport || alexReport || samReport || mariaReport);
+
+console.log(!1 && 2 && 3);
